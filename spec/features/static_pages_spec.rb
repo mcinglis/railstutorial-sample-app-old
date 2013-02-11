1fe35f8aa967c96c6476e19992e40387237168b9
Malcolm Inglis <me@minglis.id.au>
2013-02-10 19:50:30 -0500

    Finish chapter 5

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,32 +4,60 @@ feature "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector "h1", text: heading }
+    it { html.should have_selector "title", text: full_title(page_title) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector "h1", text: "Examplr" }
-    it { html.should have_selector "title", text: full_title("") }
+    let(:heading) { "Examplr" }
+    let(:page_title) { "" }
+
+    it_should_behave_like "all static pages"
     it { html.should_not have_selector "title", text: "Home" }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector "h1", text: "Help" }
-    it { html.should have_selector "title", text: full_title("Help") }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector "h1", text: "About" }
-    it { html.should have_selector "title", text: full_title("About") }
+    let(:heading) { "About" }
+    let(:page_title) { "About" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector "h1", text: "Contact" }
-    it { html.should have_selector "title", text: full_title("Contact") }
+    let(:heading) { "Contact" }
+    let(:page_title) { "Contact" }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.html.should have_selector "title", text: full_title("About")
+    click_link "Help"
+    page.html.should have_selector "title", text: full_title("Help")
+    click_link "Contact"
+    page.html.should have_selector "title", text: full_title("Contact")
+    click_link "Home"
+    click_link "Sign up"
+    page.html.should have_selector "title", text: full_title("Sign up")
+    click_link "Examplr"
+    page.html.should have_selector "title", text: "Examplr"
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,3 @@
 
-def full_title(page_title)
-  base_title = "Examplr"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
