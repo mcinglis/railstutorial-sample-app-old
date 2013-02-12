8c4dacf58154bd0323d3abbf6755a206aacfab7d
Malcolm Inglis <me@minglis.id.au>
2013-02-12 14:36:38 -0500

    Finish chapter 7 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end
